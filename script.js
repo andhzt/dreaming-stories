094cb45c00ad4765148c5357c87e3bd4bd05a10e
andhzt <00001141643133sp@al.educacao.sp.gov.br>
2024-11-19 17:45:24 -0300

    uau

---
+++ script.js
@@ -0,0 +1,7 @@
+const avanca = document.querySelectorAll(".botao-proximo")
+
+avanca.forEach(button => {
+    button.addEventListener("click", function(){
+        const atual = document.querySelector(".ativo")
+    })
+})
